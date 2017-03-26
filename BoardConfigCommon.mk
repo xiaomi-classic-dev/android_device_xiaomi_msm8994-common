a628ba86b057c2d16c439e0e07e753f55890cc78
Jeff Sharkey <jsharkey@android.com>
2017-03-25 22:57:15 -0600

    msm8994-common: Enable partitions on loop devices.
    This is used by the new "virtual disk" feature used by vold to help
    debug and test storage code on devices without physical media.
    
    Bug: 34903607
    Test: builds, boots, virtual disk works
    Change-Id: Icd43ac6f9b34241419aab17285c0afa77f0916ec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,7 +94,7 @@ USE_REDUCED_CJK_FONT_WEIGHTS := true
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/leo
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 loop.max_part=7 androidboot.selinux=permissive
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
