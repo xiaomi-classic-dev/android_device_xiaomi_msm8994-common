659a4fbcc80c5b62572888ed605328dcb23080c2
Han Wang <416810799@qq.com>
2019-09-05 07:07:51 +0200

    msm8994-common: Remove LOCAL_MODULE_TAGS := eng usage
    Change-Id: I83aa494618d9c4809d3219aa3785e56b48ef8dfb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/fstab.qcom
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
@@ -12,7 +12,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -20,7 +20,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -29,7 +29,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.qcom.rc
 LOCAL_MODULE_STEM   := ueventd.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)
@@ -39,7 +39,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.qcom.post_boot.sh
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := bin/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
@@ -47,7 +47,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.sh
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= bin/init.qcom.sh
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
@@ -55,7 +55,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.sh
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= bin/init.qcom.usb.sh
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
