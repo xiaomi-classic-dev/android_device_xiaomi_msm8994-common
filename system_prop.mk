0b6fcca3cae82b2b32a276a971a980fed87f94d8
kyoto44 <kyoto1337@protonmail.com>
2019-10-25 16:24:53 +0500

    msm8994-common: Remove deprecated display flags

--- system_prop.mk
+++ system_prop.mk
@@ -281,6 +281,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.power.useautobrightadj=true \
     persist.radio.apm_sim_not_pwdn=1
 
-# Maintainer
+# Surfaceflinger
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.mk.maintainer=guaiyihu
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=2048
+
