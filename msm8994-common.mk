2b7f32b3cd5a42cb2c36ae364bfbb82c82873356
kyoto44 <kyoto1337@protonmail.com>
2019-10-31 16:40:41 +0500

    msm8994-common: Adding Audio HAL V5: Direction API

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -97,6 +97,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@5.0 \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.$(BOARD_PLATFORM) \
