2b1998e60e232539e391219f1e9342dca5d858fe
GuaiYiHu <guaiyihu@foxmail.com>
2020-10-18 16:32:54 +0800

    msm8994-common: Build configstore hidl service
    Change-Id: Id75982d9d3d705220032f86b4e8bd97084679dcb

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     libgenlock \
+    android.hardware.configstore@1.1-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
