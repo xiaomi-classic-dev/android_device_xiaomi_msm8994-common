078e78e86dbda8d78ff5803eb64fdead88fe0465
GuaiYiHu <guaiyihu@foxmail.com>
2020-12-16 20:53:22 +0800

    msm8994-common: recovery: Clean up unused message
    Change-Id: I4c2d4225f74943b1a949b7475023866cd840d8e3

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -171,8 +171,6 @@ Value * VerifyTrustZoneFn(const char *name, State *state, const std::vector<std:
 
     ret = 0;
     for (i = 0; i < argv.size(); i++) {
-        uiPrintf(state, "Comparing TZ version %s to %s",
-                tz_version[i].c_str(), current_tz_version);
         if (strncmp(tz_version[i].c_str(), current_tz_version, tz_version[i].length()) == 0) {
             ret = 1;
             break;
