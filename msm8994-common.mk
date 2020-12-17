1028b6d63f964dbca3029db8dc5cdf00b5fb69b2
GuaiYiHu <guaiyihu@foxmail.com>
2020-12-17 11:19:03 +0800

    msm8994-common: Use libprotobuf-cpp-full-v29.so for libsettings.so
    Change-Id: I9e37331c9905ecd9b2c48c8d49bad9e6e02c6ca0

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -267,7 +267,8 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
 
 # Wifi
 PRODUCT_PACKAGES += \
