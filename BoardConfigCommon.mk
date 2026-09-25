1fca8600e52a314e84e42ad506da129945db8405
GuaiYiHu <guaiyihu@foxmail.com>
2026-09-25 18:23:03 +0800

    msm8994-common: Declare Android 14 FCM level and legacy HALs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,6 +154,9 @@ TARGET_PER_MGR_ENABLED := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
