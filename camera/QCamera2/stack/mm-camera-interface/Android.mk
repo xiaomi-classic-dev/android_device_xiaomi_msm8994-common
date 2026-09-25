c8b04a0e5b8a3ba623b7843f6e51ea63900edcc7
GuaiYiHu <guaiyihu@foxmail.com>
2026-09-25 18:08:53 +0800

    msm8994-common: Drop obsolete camera header copy rule

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -19,10 +19,6 @@ ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 msm8916 apq8084 msm8084 msm8994
 endif
 
 LOCAL_CFLAGS += -D_ANDROID_
-LOCAL_COPY_HEADERS_TO := mm-camera-interface
-LOCAL_COPY_HEADERS += ../common/cam_intf.h
-LOCAL_COPY_HEADERS += ../common/cam_types.h
-
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
