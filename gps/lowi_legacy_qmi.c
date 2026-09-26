7fb8d0b68a9b1d6a585fc2c152d279bd88acb6a6
GuaiYiHu <guaiyihu@foxmail.com>
2026-09-26 10:59:49 +0800

    msm8994-common: restore LOWI legacy QMI access

--- gps/lowi_legacy_qmi.c
+++ gps/lowi_legacy_qmi.c
@@ -4,7 +4,8 @@
 int main(int argc, char *argv[])
 {
     (void)argc;
-    if (setenv("LD_LIBRARY_PATH", "/vendor/lib64/gps_legacy:/vendor/lib64", 1) != 0)
+    if (setenv("LD_LIBRARY_PATH",
+               "/vendor/lib64/gps_legacy:/system/lib64:/vendor/lib64", 1) != 0)
         return 1;
 
     argv[0] = "/vendor/bin/lowi-server";
